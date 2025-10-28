487ba19d1b1b6d4e3313de1d2dceb5f4247dafdf
Kwan Yuet Stephen Ho <3810067+stephenhky@users.noreply.github.com>
2025-10-28 15:04:09 -0400

    board implementation continued

--- src/animalchess/chess/board.py
+++ src/animalchess/chess/board.py
@@ -3,6 +3,7 @@ from typing import Self, Literal, Generator
 from dataclasses import dataclass
 
 import numpy as np
+from loguru import logger
 
 from .utils import SquareType, Piece, AnimalType, AnimalChessBoardMap
 from .utils import BOARD_HEIGHT, BOARD_WIDTH
@@ -283,7 +284,6 @@ class ElephantPiece(Piece):
 class PieceInformation:
     piece: Piece
     position: tuple[int, int]
-    alive: bool = True
 
 
 class PlayerPossession:
@@ -322,7 +322,7 @@ class PlayerPossession:
 
     def iterate_living_pieces(self) -> Generator[PieceInformation, None, None]:
         for animal_piece_info in self._pieces.values():
-            if animal_piece_info.alive:
+            if not animal_piece_info.piece.dead:
                 yield animal_piece_info
 
     @property
@@ -331,19 +331,36 @@ class PlayerPossession:
 
 
 class AnimalChessBoard:
-    def __init__(self, player1: Player, player2: Player):
+    def __init__(self, player0: Player, player1: Player):
         self._map = AnimalChessBoardMap()
+        self._player0 = player0
         self._player1 = player1
-        self._player2 = player2
         self.initialize_board()
 
     def initialize_board(self) -> None:
-        self._player1_possession = PlayerPossession(self._player1, 0)
-        self._player2_possession = PlayerPossession(self._player2, 1)
+        self._players_possessions = [
+            PlayerPossession(self._player0, 0),
+            PlayerPossession(self._player1, 1)
+        ]
 
         self._board = np.empty((BOARD_HEIGHT, BOARD_WIDTH), dtype=object)
-        for animal_piece_info in self._player1_possession.iterate_living_pieces():
-            self._board[*animal_piece_info.position] = animal_piece_info.piece
-        for animal_piece_info in self._player2_possession.iterate_living_pieces():
-            self._board[*animal_piece_info.position] = animal_piece_info.piece
+        for possession in self._players_possessions:
+            for animal_piece_info in possession.iterate_living_pieces():
+                self._board[*animal_piece_info.position] = animal_piece_info.piece
+
+    def move_piece(
+            self,
+            player_id: Literal[0, 1],
+            animal: AnimalType,
+            destination: tuple[int, int]
+    ) -> bool:   # success: True; failed: False
+        piece_info = self._players_possessions[player_id].get_piece(animal)
+        if piece_info.piece.dead:
+            logger.info(f"Player {self._players_possessions[player_id].player.name}: {piece_info.piece.animal_type.name} is dead")
+            return False
+
+        if not piece_info.piece.is_valid_move():
+            logger.info("Not a valid move.")
+            return False
+
 
