d029e9d8dafd57a562a70eab05f4274e89e730d6
stephenhky <stephenhky@yahoo.com.hk>
2025-11-24 14:30:13 -0500

    test exhaustively actions

---
+++ test/test_list_actions.py
@@ -0,0 +1,52 @@
+
+from itertools import product
+import unittest
+
+from animalchess.chess.board import AnimalChessBoard, PlayerPossession, BOARD_HEIGHT, BOARD_WIDTH
+from animalchess.chess.player import Player
+from animalchess.chess.utils import AnimalType, AnimalChessBoardMap, SquareType
+
+
+class TestActions(unittest.TestCase):
+    def setUp(self):
+        self.player0 = Player("Player 0")
+        self.player1 = Player("Player 1")
+        self.board = AnimalChessBoard(self.player0, self.player1)
+
+    def test_corner(self):
+        # Create a custom board for eating tests
+        player0_possession = PlayerPossession(self.player0, 0, reset=False)
+        player1_possession = PlayerPossession(self.player1, 1, reset=False)
+        player0_possession.set_piece_info(AnimalType.LION, (0, 0))
+        player1_possession.set_piece_info(AnimalType.LION, (8, 6))
+        board = AnimalChessBoard(
+            self.player0,
+            self.player1,
+            initial_players_possessions=[player0_possession, player1_possession]
+        )
+
+        player0_lion_possible_destinations = [(0, 1), (1, 0)]
+        player0_system_lion_possible_destinations = [
+            (new_i, new_j)
+            for new_i, new_j in board.exhaustively_iterate_available_destinations(0, AnimalType.LION)
+        ]
+        for i, j in product(range(BOARD_HEIGHT), range(BOARD_WIDTH)):
+            if (i, j) in player0_lion_possible_destinations:
+                self.assertIn((i, j), player0_system_lion_possible_destinations)
+            else:
+                self.assertFalse((i, j) in player0_system_lion_possible_destinations)
+
+        player1_lion_possible_destinations = [(8, 5), (7, 6)]
+        player1_system_lion_possible_destinations = [
+            (new_i, new_j)
+            for new_i, new_j in board.exhaustively_iterate_available_destinations(1, AnimalType.LION)
+        ]
+        for i, j in product(range(BOARD_HEIGHT), range(BOARD_WIDTH)):
+            if (i, j) in player1_lion_possible_destinations:
+                self.assertIn((i, j), player1_system_lion_possible_destinations)
+            else:
+                self.assertFalse((i, j) in player1_system_lion_possible_destinations)
+
+
+if __name__ == '__main__':
+    unittest.main()
