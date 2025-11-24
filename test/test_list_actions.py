b12c5299cb798c6276330f5a8e5c1312606297dc
stephenhky <stephenhky@yahoo.com.hk>
2025-11-24 15:23:28 -0500

    test ordinary moves

--- test/test_list_actions.py
+++ test/test_list_actions.py
@@ -92,7 +92,38 @@ class TestActions(unittest.TestCase):
             else:
                 self.assertFalse((i, j) in player0_system_rat_possible_destinations)
 
+    def test_ordinary_moves(self):
+        player0_possession = PlayerPossession(self.player0, 0, reset=False)
+        player1_possession = PlayerPossession(self.player1, 1, reset=False)
+        player0_possession.set_piece_info(AnimalType.DOG, (1, 1))
+        player1_possession.set_piece_info(AnimalType.CAT, (7, 1))
+        board = AnimalChessBoard(
+            self.player0,
+            self.player1,
+            initial_players_possessions=[player0_possession, player1_possession]
+        )
 
+        player0_dog_possible_destinations = [(0, 1), (1, 0), (1, 2), (2, 1)]
+        player0_system_dog_possible_destinations = [
+            (new_i, new_j)
+            for new_i, new_j in board.exhaustively_iterate_available_destinations(0, AnimalType.DOG)
+        ]
+        for i, j in product(range(BOARD_HEIGHT), range(BOARD_WIDTH)):
+            if (i, j) in player0_dog_possible_destinations:
+                self.assertIn((i, j), player0_system_dog_possible_destinations)
+            else:
+                self.assertFalse((i, j) in player0_system_dog_possible_destinations)
+
+        player1_cat_possible_destinations = [(6, 1), (8, 1), (7, 0), (7, 2)]
+        player1_system_cat_possible_destinations = [
+            (new_i, new_j)
+            for new_i, new_j in board.exhaustively_iterate_available_destinations(1, AnimalType.CAT)
+        ]
+        for i, j in product(range(BOARD_HEIGHT), range(BOARD_WIDTH)):
+            if (i, j) in player1_cat_possible_destinations:
+                self.assertIn((i, j), player1_system_cat_possible_destinations)
+            else:
+                self.assertFalse((i, j) in player1_system_cat_possible_destinations)
 
 
 if __name__ == '__main__':
