b9eb0de6913831b4b34e945614c36f962e07008d
stephenhky <stephenhky@yahoo.com.hk>
2025-11-24 15:45:51 -0500

    fixed animals

--- test/test_list_actions.py
+++ test/test_list_actions.py
@@ -152,6 +152,63 @@ class TestActions(unittest.TestCase):
         player1_lion_possible_destinations = [(0, 0), (1, 1)]
         player1_system_lion_possible_destinations = [
             (new_i, new_j)
+            for new_i, new_j in board.exhaustively_iterate_available_destinations(1, AnimalType.LION)
+        ]
+        for i, j in product(range(BOARD_HEIGHT), range(BOARD_WIDTH)):
+            if (i, j) in player1_lion_possible_destinations:
+                self.assertIn((i, j), player1_system_lion_possible_destinations)
+            else:
+                self.assertFalse((i, j) in player1_system_lion_possible_destinations)
+
+        player0_rat_possible_destinations = [(1, 3), (2, 4), (1, 5), (0, 4)]
+        player0_system_rat_possible_destinations = [
+            (new_i, new_j)
+            for new_i, new_j in board.exhaustively_iterate_available_destinations(0, AnimalType.RAT)
+        ]
+        for i, j in product(range(BOARD_HEIGHT), range(BOARD_WIDTH)):
+            if (i, j) in player0_rat_possible_destinations:
+                self.assertIn((i, j), player0_system_rat_possible_destinations)
+            else:
+                self.assertFalse((i, j) in player0_system_rat_possible_destinations)
+
+        player1_tiger_possible_destinations = [(1, 2), (1, 4), (2, 3), (0, 3)]
+        player1_system_tiger_possible_destinations = [
+            (new_i, new_j)
+            for new_i, new_j in board.exhaustively_iterate_available_destinations(1, AnimalType.TIGER)
+        ]
+        for i, j in product(range(BOARD_HEIGHT), range(BOARD_WIDTH)):
+            if (i, j) in player1_tiger_possible_destinations:
+                self.assertIn((i, j), player1_system_tiger_possible_destinations)
+            else:
+                self.assertFalse((i, j) in player1_system_tiger_possible_destinations)
+
+    def test_player1_cave_trap(self):
+        player0_possession = PlayerPossession(self.player0, 0, reset=False)
+        player1_possession = PlayerPossession(self.player1, 1, reset=False)
+        player0_possession.set_piece_info(AnimalType.DOG, (0, 2))
+        player1_possession.set_piece_info(AnimalType.LION, (0, 1))
+        player0_possession.set_piece_info(AnimalType.RAT, (1, 4))
+        player1_possession.set_piece_info(AnimalType.TIGER, (1, 3))
+        board = AnimalChessBoard(
+            self.player0,
+            self.player1,
+            initial_players_possessions=[player0_possession, player1_possession]
+        )
+
+        player0_dog_possible_destinations = [(1, 2)]
+        player0_system_dog_possible_destinations = [
+            (new_i, new_j)
+            for new_i, new_j in board.exhaustively_iterate_available_destinations(0, AnimalType.DOG)
+        ]
+        for i, j in product(range(BOARD_HEIGHT), range(BOARD_WIDTH)):
+            if (i, j) in player0_dog_possible_destinations:
+                self.assertIn((i, j), player0_system_dog_possible_destinations)
+            else:
+                self.assertFalse((i, j) in player0_system_dog_possible_destinations)
+
+        player1_lion_possible_destinations = [(0, 0), (1, 1)]
+        player1_system_lion_possible_destinations = [
+            (new_i, new_j)
             for new_i, new_j in board.exhaustively_iterate_available_destinations(1, AnimalType.CAT)
         ]
         for i, j in product(range(BOARD_HEIGHT), range(BOARD_WIDTH)):
